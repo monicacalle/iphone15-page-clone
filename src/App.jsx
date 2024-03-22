74a1667a15a46a26049821a65f6ea316074d1e82
monica <monicacalle369@gmail.com>
2024-03-22 18:06:23 +0100

    first animations

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      {/* <Model />
+      <Features />
+      <HowItWorks />
+      <Footer /> */}
+    </main>
+  );
+}
+
+export default App;
