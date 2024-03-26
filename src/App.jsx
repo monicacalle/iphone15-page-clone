68753c2553f5a4a37e17209fddd9b15b0cc9412e
monica <monicacalle369@gmail.com>
2024-03-26 11:35:47 +0100

    create animations with gsap and three

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import HowItWorks from "./components/HowItWorks";
+import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
-      {/* <Model />
+      <Model />
       <Features />
       <HowItWorks />
-      <Footer /> */}
+      <Footer />
     </main>
   );
-}
+};
 
 export default App;
